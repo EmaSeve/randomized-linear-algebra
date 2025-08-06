44f55deb0e6067186c69fb63f3781890e7b2b580
NicoNovi9 <nicolanoventa9@gmail.com>
2025-08-06 09:34:11 +0200

    Remove unnecessary comments and improve code clarity in StochasticLA implementation

--- include/StochasticLA_impl.hpp
+++ include/StochasticLA_impl.hpp
@@ -8,14 +8,12 @@ namespace StochasticLA {
 template<typename FloatType>
 typename RandomizedLinearAlgebra<FloatType>::Matrix 
 RandomizedLinearAlgebra<FloatType>::multiply(const Matrix& A, const Matrix& B) {
-    // Simple wrapper around Eigen's matrix multiplication
     return A * B;
 }
 
 template<typename FloatType>
 typename RandomizedLinearAlgebra<FloatType>::Scalar 
 RandomizedLinearAlgebra<FloatType>::frobeniusNorm(const Matrix& A) {
-    // Use Eigen's built-in Frobenius norm
     return A.norm();
 }
 
@@ -24,7 +22,6 @@ typename RandomizedLinearAlgebra<FloatType>::Matrix
 RandomizedLinearAlgebra<FloatType>::randomMatrix(int rows, int cols, int seed) {
     Matrix result(rows, cols);
     
-    // Set up random number generator
     std::mt19937 gen;
     if (seed >= 0) {
         gen.seed(seed);
@@ -34,7 +31,6 @@ RandomizedLinearAlgebra<FloatType>::randomMatrix(int rows, int cols, int seed) {
     
     std::uniform_real_distribution<FloatType> dist(0.0, 1.0);
     
-    // Fill matrix with random values
     for (int i = 0; i < rows; ++i) {
         for (int j = 0; j < cols; ++j) {
             result(i, j) = dist(gen);
